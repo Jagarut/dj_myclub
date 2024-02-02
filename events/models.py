d401781c27985dc24eb300f826ec415513d95cc7
Jagarut <jagaruc@gmail.com>
2024-02-02 18:54:39 +0000

    correct some bugs

--- events/models.py
+++ events/models.py
@@ -2,7 +2,10 @@ from django.db import models
 
 class Event(models.Model):
     name = models.CharField('Event Name', max_length=120)
-    date = models.DateTimeField('Event Date')
+    event_date = models.DateTimeField('Event Date')
     venue = models.CharField(max_length=120)
     manager = models.CharField(max_length=60)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
