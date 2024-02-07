e4944da6e8723fc7d38b9a68a3c4a01ad90d1343
Chus <jagaruc@gmail.com>
2024-02-07 22:04:39 +0000

    add url

--- events/views.py
+++ events/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from datetime import date
 
-# Create your views here.
+def index(request, year, month):
+    # t = date.today()
+    # month = date.strftime(t, "%b")
+    # year = t.year
+    title = f"My Club Event Calendar - {month} {year}"
+    return HttpResponse("<h1>%s</h1>"%title )
