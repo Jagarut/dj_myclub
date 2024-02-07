c11894185b4088a8d02e4330f9c52b8703f587ab
Jagarut <jagaruc@gmail.com>
2024-02-07 23:33:35 +0000

    modify index view

--- events/views.py
+++ events/views.py
@@ -1,10 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from datetime import date
+import calendar
+from calendar import HTMLCalendar
 
-def index(request, year, month):
+def index(request, year=date.today().year, month=date.today().month):
     # t = date.today()
     # month = date.strftime(t, "%b")
-    # year = t.year
-    title = f"My Club Event Calendar - {month} {year}"
-    return HttpResponse("<h1>%s</h1>"%title )
+    year = int(year)
+    month = int(month)
+    if year < 2000 or year > 2099:
+        year = date.today().year
+
+    month_name = calendar.month_name[month]
+
+    title = f"My Club Event Calendar - {month_name} {year}"
+    cal = HTMLCalendar().formatmonth(year, month)
+    return HttpResponse("<h1>%s</h1><p>%s</p>"%(title,cal) )
