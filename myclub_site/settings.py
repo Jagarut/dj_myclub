6940a529bd8c9d8e0f1e26e403fe4144b7a25bad
Jagarut <jagaruc@gmail.com>
2024-02-08 22:08:54 +0000

    add logo banner and main.css

--- myclub_site/settings.py
+++ myclub_site/settings.py
@@ -77,7 +77,7 @@ WSGI_APPLICATION = 'myclub_site.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR/'db.sqlite3',
     }
 }
 
@@ -117,6 +117,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [BASE_DIR / 'myclub_site/static']
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
