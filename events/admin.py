a37bb974e16bcf2cba0b0dcd27d991872612c117
Jagarut <jagaruc@gmail.com>
2024-02-12 18:41:23 +0000

    update the Event Model

--- events/admin.py
+++ events/admin.py
@@ -9,4 +9,11 @@ class VenueAdmin(admin.ModelAdmin):
 
 # admin.site.register(Venue)
 admin.site.register(MyClubUser)
-admin.site.register(Event)
+
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    fields = (('name','venue'), 'event_date', 'description', 'manager')
+    list_display = ('name', 'event_date', 'venue')    
+    list_filter = ('event_date', 'venue')
+    ordering = ('-event_date',)
+# admin.site.register(Event)
