50c2c2b2f8cb577ceac28b7700bb596dce176194
Chus <jagaruc@gmail.com>
2024-02-14 19:51:41 +0000

    add contact form

--- myclub_site/urls.py
+++ myclub_site/urls.py
@@ -17,9 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
+from . import contact
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('contact/', contact.contact, name='contact'),
     path('admin/password_reset/', auth_views.PasswordResetView.as_view(), name='admin_password_reset'),
     path('admin/password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
     path('reset/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
